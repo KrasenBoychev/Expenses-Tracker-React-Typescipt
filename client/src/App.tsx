6f6d11d230039656aef8aeef83705a89e85be584
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-05 16:15:41 +0100

    App.tsx imports Header, Footer, HomeNoAuth and Login components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,20 +3,27 @@ import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import HomeNoAuth from "./components/home/HomeNoAuth";
+import Login from "./components/authentication/Login";
+import { AuthContextProvider } from "./contexts/AuthContext";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div className="max-w-3xl flex flex-col mx-auto size-full">
-      <Header />
+    <AuthContextProvider>
+      <Toaster />
+      <div className="max-w-3xl flex flex-col mx-auto size-full">
+        <Header />
 
-      <main id="content">
-        <Routes>
-          <Route path="/" element={<HomeNoAuth />} />
-        </Routes>
-      </main>
+        <main id="content">
+          <Routes>
+            <Route path="/" element={<HomeNoAuth />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </AuthContextProvider>
   );
 }
 
