8c009c927cc6711c714013f5e0cbb835abf87e54
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-05 19:53:22 +0100

    sandwich menu opens and closes based on toogleNav state; pages highlight based on page location

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,13 +1,22 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const [toggleNav, setToggleNav] = useState<boolean>(false);
+  const [currPage, setCurrPage] = useState<string>("");
+  const location = useLocation();
+
+  useEffect(() => {
+    setCurrPage(location.pathname);
+  }, [location]);
+
   return (
     <header className="mb-auto flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full text-sm py-4">
-      <nav className="w-full px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
+      <nav className="w-full px-4 sm:flex sm:items-center sm:justify-between gap-10 sm:px-2 lg:px-4">
         <div className="flex items-center justify-between">
           <Link
             to="/"
-            className="flex-none text-xl font-semibold text-white focus:outline-hidden focus:opacity-80"
+            className="flex-none text-xl font-semibold text-white focus:outline-hidden"
             aria-label="Brand"
           >
             Expenses Tracker
@@ -53,38 +62,53 @@ export default function Header() {
                 <path d="M18 6 6 18" />
                 <path d="m6 6 12 12" />
               </svg>
-              <span className="sr-only">Toggle navigation</span>
+              <span
+                className="sr-only"
+                onClick={() => setToggleNav(!toggleNav)}
+              >
+                Toggle navigation
+              </span>
             </button>
           </div>
         </div>
         <div
           id="hs-navbar-cover-page"
-          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
+          className={`hs-collapse ${
+            !toggleNav && "hidden"
+          } overflow-hidden transition-all duration-300 grow sm:block`}
           aria-labelledby="hs-navbar-cover-page-collapse"
         >
           <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
             <Link
               to="/"
-              className="font-medium text-white focus:outline-hidden"
-              aria-current="page"
+              className={`font-medium hover:text-white ${
+                currPage === "/" ? "text-white" : "text-white/70"
+              }`}
+              // aria-current="page"
             >
               Home
             </Link>
             <Link
               to="/about"
-              className="font-medium text-white/70 hover:text-white focus:outline-hidden focus:text-white"
+              className={`font-medium hover:text-white ${
+                currPage === "/about" ? "text-white" : "text-white/70"
+              }`}
             >
               About
             </Link>
             <Link
               to="/login"
-              className="font-medium text-white/70 hover:text-white focus:outline-hidden focus:text-white"
+              className={`font-medium hover:text-white ${
+                currPage === "/login" ? "text-white" : "text-white/70"
+              }`}
             >
               Login
             </Link>
             <Link
               to="/register"
-              className="font-medium text-white/70 hover:text-white focus:outline-hidden focus:text-white"
+              className={`font-medium hover:text-white ${
+                currPage === "/register" ? "text-white" : "text-white/70"
+              }`}
             >
               Register
             </Link>
