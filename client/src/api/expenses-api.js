87b5caf2753122413bed67da44c41b8a78d303dd
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-05 16:05:25 +0100

    epenses-api created

---
+++ client/src/api/expenses-api.js
@@ -0,0 +1,7 @@
+import * as api from './requester.js';
+
+const host = api.settings.host;
+
+// export async function getCatalogLights() {
+//     return await api.get(host + '/data/catalog/' + api.adminId);
+// }
