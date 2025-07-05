60d6655bb4e5848a96c524e8160cb5f94f7b4aac
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-05 16:13:04 +0100

    authentication.ts - includes interfaces for all components related to the authentication process

---
+++ client/src/interfaces/authentication.ts
@@ -0,0 +1,27 @@
+export interface DataRequesterInterface extends FormErrorsInterface { }
+
+export interface AuthDataInterface {
+    userId: string,
+    email: string,
+    accessToken: string,
+}
+
+export interface ContextDataInterface {
+    userId: string,
+    email: string,
+    accessToken: string,
+    isAuthenticated: boolean,
+    changeAuthState: Function,
+    logout: Function,
+}
+
+export interface InitialValuesInterface {
+    email: string,
+    password: string,
+}
+
+export interface FormErrorsInterface {
+    email?: string,
+    password?: string,
+    rePass?: string
+}
