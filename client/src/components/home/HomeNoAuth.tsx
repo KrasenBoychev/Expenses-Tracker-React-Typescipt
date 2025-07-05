e07bf57c7f7fa6649cf138e4385d83dc9a45a7d6
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-05 19:52:14 +0100

    HomeNoAuth.jsx updated

--- client/src/components/home/HomeNoAuth.tsx
+++ client/src/components/home/HomeNoAuth.tsx
@@ -1,21 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function HomeNoAuth() {
+  const rotateSVG = {
+    transform: "rotate(180deg)",
+  };
   return (
     <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
-      <h1 className="block text-2xl font-bold text-white sm:text-4xl">
-        Cover Page
+      <h1 className="block text-2xl font-bold text-white sm:text-4xl uppercase">
+        Welcome to Expenses Tracker
       </h1>
-      <p className="mt-3 text-lg text-gray-300">
-        Cover is a one-page template for building simple and beautiful home
-        pages using Tailwind CSS.
-      </p>
-      <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
-        <a
+      <div className="mt-10 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
+        <Link
+          to="/login"
           className="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-white text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none"
-          target="parent"
-          href="../examples.html"
         >
+          Account
           <svg
             className="shrink-0 size-4"
+            style={rotateSVG}
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
@@ -28,8 +30,7 @@ export default function HomeNoAuth() {
           >
             <path d="m15 18-6-6 6-6" />
           </svg>
-          Back to examples
-        </a>
+        </Link>
       </div>
     </div>
   );
