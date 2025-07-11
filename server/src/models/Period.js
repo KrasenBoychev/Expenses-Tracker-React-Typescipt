faef128558238242c30a8caf614265d5cbf59718
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-11 17:50:00 +0100

    Period Model created

---
+++ server/src/models/Period.js
@@ -0,0 +1,31 @@
+const { Schema, model, Types } = require("mongoose");
+
+const PeriodSchema = new Schema({
+  startDate: {
+    type: Date,
+    default: Date.now,
+  },
+  endDate: {
+    type: Date,
+  },
+  income: {
+    type: Number,
+    default: 0,
+  },
+  plannedExpenses: {
+    type: Array,
+    default: [],
+  },
+  actualExpenses: {
+    type: Array,
+    default: [],
+  },
+  budgetId: {
+    type: Types.ObjectId,
+    ref: "Budget",
+  },
+});
+const Period = model("periods", PeriodSchema);
+Period.createIndexes();
+
+module.exports = { Period };
