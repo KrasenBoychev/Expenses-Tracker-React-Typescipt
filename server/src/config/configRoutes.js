38e763f738cd6f97772055d7ec7161c1de06f338
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-11 17:51:20 +0100

    budget route created

--- server/src/config/configRoutes.js
+++ server/src/config/configRoutes.js
@@ -1,13 +1,9 @@
-const { userRouter } = require('../controllers/user');
-const { lightsRouter } = require('../controllers/lights');
-const { commentsRouter } = require('../controllers/comments');
-const { cartRouter } = require('../controllers/cart');
+const { userRouter } = require("../controllers/user");
+const { budgetRouter } = require("../controllers/budget");
 
 function configRoutes(app) {
-  app.use('/users', userRouter);
-  app.use('/data', lightsRouter);
-  app.use('/comments', commentsRouter);
-  app.use('/cart', cartRouter);
+  app.use("/users", userRouter);
+  app.use("/budget", budgetRouter);
 }
 
 module.exports = { configRoutes };
