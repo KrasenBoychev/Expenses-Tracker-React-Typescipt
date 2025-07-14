7028eda16e885827ff7cf2173580d5633d85095c
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-14 18:41:04 +0100

    budget and periods are set when the component renders and periods value is passed to StickySidebar and BudgetOverview components

--- client/src/components/budget/BudgetMiddleware.tsx
+++ client/src/components/budget/BudgetMiddleware.tsx
@@ -1,25 +1,40 @@
 import { useLocation } from "react-router-dom";
 import StickySidebar from "../header & nav/StickySidebar";
 import { useEffect, useState } from "react";
-import { getSingleBudget } from "../../api/expenses-api";
+import {
+  createNewPeriod,
+  getAllPeriods,
+  getSingleBudget,
+} from "../../api/expenses-api";
 import toast from "react-hot-toast";
 import BudgetOverview from "./BudgetOverview";
 import BudgetAddIncomeExpense from "./BudgetAddIncomeExpense";
 import BudgetCards from "./BudgetCards";
 import BudgetMembers from "./BudgetMembers";
-import type { BudgetInterface } from "../../interfaces/budget";
+import type { BudgetInterface, PeriodInterface } from "../../interfaces/budget";
 
 export default function BudgetMiddleware() {
   const [pageToRender, setPageToRender] = useState<string>("budgetOverview");
   const [budget, setBudget] = useState<BudgetInterface | null>(null);
+  const [periods, setPeriods] = useState<PeriodInterface[] | null>(null);
   const location = useLocation();
   const budgetId = location.pathname.split("/")[1];
 
   useEffect(() => {
     (async () => {
       try {
-        const getBudget = await getSingleBudget(budgetId);
-        setBudget(getBudget[0]);
+        const getBudgetResponse = await getSingleBudget(budgetId);
+        const getBudget: BudgetInterface = getBudgetResponse[0];
+        const getPeriods = await getAllPeriods(getBudget.allPeriods);
+
+        if (getPeriods.length == 0) {
+          const newPeriod = await createNewPeriod(new Date(), getBudget._id);
+          setPeriods(newPeriod);
+        } else {
+          setPeriods(getPeriods);
+        }
+
+        setBudget(getBudget);
       } catch (e: unknown) {
         if (e instanceof Error) {
           return toast.error(e.message);
@@ -29,8 +44,14 @@ export default function BudgetMiddleware() {
   }, []);
   return (
     <>
-      <StickySidebar setPageToRender={setPageToRender} budget={budget} />
-      {pageToRender == "budgetOverview" && <BudgetOverview budget={budget} />}
+      <StickySidebar
+        setPageToRender={setPageToRender}
+        budget={budget}
+        periods={periods}
+      />
+      {pageToRender == "budgetOverview" && (
+        <BudgetOverview budget={budget} periods={periods} />
+      )}
       {pageToRender == "addIncomeOrExpense" && (
         <BudgetAddIncomeExpense budget={budget} />
       )}
