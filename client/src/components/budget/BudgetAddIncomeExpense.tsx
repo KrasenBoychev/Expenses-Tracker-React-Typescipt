d952c4a3fde07acf111cbf6a70896e06ad149870
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-14 08:43:30 +0100

    BudgetAddIncomeExpense created

---
+++ client/src/components/budget/BudgetAddIncomeExpense.tsx
@@ -0,0 +1,7 @@
+import type { BudgetMiddlewarePropInterface } from "../../interfaces/budget";
+
+export default function BudgetAddIncomeExpense({ budget }: BudgetMiddlewarePropInterface) {
+  return (
+    <div>BudgetAddIncomeExpense</div>
+  )
+}
