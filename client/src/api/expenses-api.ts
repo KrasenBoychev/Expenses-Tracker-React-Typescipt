6cd903f2c88cfc85993789d1c9da55eed8fde141
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-14 08:37:38 +0100

    "getSingleBudget" api call created

--- client/src/api/expenses-api.ts
+++ client/src/api/expenses-api.ts
@@ -6,6 +6,10 @@ export async function getAllBudgets(budgetsIds: string[]) {
   return await api.post(host + "/budget/userBudgets", { budgetsIds });
 }
 
+export async function getSingleBudget(budgetId: string) {
+  return await api.post(host + "/budget/singleBudget", { budgetId });
+}
+
 export async function createNewBudget(budgetName: string) {
   return await api.post(host + "/budget/newBudget", { budgetName });
 }
