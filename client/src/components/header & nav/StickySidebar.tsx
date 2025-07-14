63d0f1c8b0f39642482804a26358a61f71eee911
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-14 09:17:14 +0100

    StickySidebarPropsInterface type for the props

--- client/src/components/header & nav/StickySidebar.tsx
+++ client/src/components/header & nav/StickySidebar.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
-import type { SetPageToRenderPropInterface } from "../../interfaces/budget";
+import type { StickySidebarPropsInterface } from "../../interfaces/budget";
 
 export default function StickySidebar({
   setPageToRender,
-}: SetPageToRenderPropInterface) {
+  budget,
+}: StickySidebarPropsInterface) {
   return (
     <>
       <div
@@ -51,7 +52,7 @@ export default function StickySidebar({
                     onClick={() => setPageToRender("budgetOverview")}
                   >
                     <i className="fa-solid fa-money-bill-wave"></i>
-                    BudgetName Overview
+                    {budget?.budgetName} overview
                   </p>
                 </li>
 
@@ -88,7 +89,7 @@ export default function StickySidebar({
                 <li>
                   <p className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-neutral-200 hs-scrollspy-active:bg-gray-100 dark:hs-scrollspy-active:bg-neutral-700">
                     <i className="fa-solid fa-piggy-bank"></i>
-                    Total Savings: number
+                    Total Savings: number??
                   </p>
                 </li>
 
