5670ce7b308b67effe2007901a020cb0f28ecee9
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-14 09:16:33 +0100

    BudgetOverview updated

--- client/src/components/budget/BudgetOverview.tsx
+++ client/src/components/budget/BudgetOverview.tsx
@@ -1,5 +1,57 @@
 import type { BudgetMiddlewarePropInterface } from "../../interfaces/budget";
 
-export default function BudgetOverview({ budget }: BudgetMiddlewarePropInterface) {
-  return <div>{budget?.budgetName}</div>;
+export default function BudgetOverview({
+  budget,
+}: BudgetMiddlewarePropInterface) {
+  return (
+    // <!-- Features -->
+    <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      {/* <!-- Grid --> */}
+      <div className="grid gap-6 grid-cols-2 sm:gap-12 lg:grid-cols-3 lg:gap-8">
+        {/* <!-- Stats --> */}
+        <div>
+          <h4 className="text-lg sm:text-xl font-semibold text-white uppercase">
+            Income
+          </h4>
+          <p className="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">
+            99.95%
+          </p>
+          <p className="mt-1 text-gray-500 dark:text-neutral-500">
+            in fulfilling orders
+          </p>
+        </div>
+        {/* <!-- End Stats --> */}
+
+        {/* <!-- Stats --> */}
+        <div>
+          <h4 className="text-lg sm:text-xl font-semibold text-white uppercase">
+            Planned to spend
+          </h4>
+          <p className="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">
+            2,000+
+          </p>
+          <p className="mt-1 text-gray-500 dark:text-neutral-500">
+            partner with Preline
+          </p>
+        </div>
+        {/* <!-- End Stats --> */}
+
+        {/* <!-- Stats --> */}
+        <div>
+          <h4 className="text-lg sm:text-xl font-semibold text-white uppercase">
+            Planned to save
+          </h4>
+          <p className="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">
+            85%
+          </p>
+          <p className="mt-1 text-gray-500 dark:text-neutral-500">
+            this year alone
+          </p>
+        </div>
+        {/* <!-- End Stats --> */}
+      </div>
+      {/* <!-- End Grid --> */}
+    </div>
+    // <!-- End Features -->
+  );
 }
