2ad27dc34a32eceabebf82b42ce4e0e86e3785f0
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-14 08:41:09 +0100

    BudgetCard navigates to the selected budget and shows the number of members having access to the budget

--- client/src/components/home/BudgetCard.tsx
+++ client/src/components/home/BudgetCard.tsx
@@ -1,14 +1,19 @@
-import type {
-  BudgetPropInterface,
-} from "../../interfaces/budget";
+import { useNavigate } from "react-router-dom";
+import type { BudgetPropInterface } from "../../interfaces/budget";
 
 export default function BudgetCard({ budget }: BudgetPropInterface) {
+  const navigate = useNavigate();
   return (
-    <div className="flex flex-col gap-8 w-[250px] h-[270px] text-center bg-white border border-gray-200 shadow-2xs rounded-xl">
-      <h3 className="text-xl font-semibold text-blue-600">
+    <div
+      className="flex flex-col gap-8 w-[250px] h-[270px] justify-center text-center bg-white border border-gray-200 shadow-2xs rounded-xl hover:opacity-80 cursor-pointer"
+      onClick={() => navigate(`/${budget._id}`)}
+    >
+      <h3 className="text-2xl font-semibold text-blue-600">
         {budget.budgetName}
       </h3>
-      <p className="mt-3 text-gray-500 dark:text-neutral-500 rounded"></p>
+      <p className="mt-3 text-gray-500 dark:text-neutral-500 rounded">
+        Members: {budget.members.length}
+      </p>
     </div>
   );
 }
