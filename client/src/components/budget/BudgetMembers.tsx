1de7250e20f4578da1c59a749a5fa8044c020984
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-14 08:43:12 +0100

    BudgetMembers created

---
+++ client/src/components/budget/BudgetMembers.tsx
@@ -0,0 +1,7 @@
+import type { BudgetMiddlewarePropInterface } from "../../interfaces/budget";
+
+export default function BudgetMembers({ budget }: BudgetMiddlewarePropInterface) {
+  return (
+    <div>BudgetMembers</div>
+  )
+}
