e9eb37b2b60b3b74d0ef7e06416cecaffe7dd80a
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-14 18:37:45 +0100

    PeriodInterface, ExpenseInterface,BudgetAndPeriodsPropsInterface created

--- client/src/interfaces/budget.ts
+++ client/src/interfaces/budget.ts
@@ -6,6 +6,21 @@ export interface BudgetInterface {
     members: string[],
 }
 
+export interface PeriodInterface {
+    _id: string,
+    startDate: Date,
+    endDate: Date,
+    income: number,
+    plannedExpenses: ExpenseInterface[],
+    actualExpenses: ExpenseInterface[],
+    budgetId: string,
+}
+
+export interface ExpenseInterface {
+    expenseType: string,
+    value: number,
+}
+
 export interface BudgetPropInterface {
     budget: BudgetInterface,
 }
@@ -14,7 +29,13 @@ export interface BudgetMiddlewarePropInterface {
     budget: BudgetInterface | null,
 }
 
+export interface BudgetAndPeriodsPropsInterface {
+    budget: BudgetInterface | null,
+    periods: PeriodInterface[] | null
+}
+
 export interface StickySidebarPropsInterface {
     setPageToRender: Function,
     budget: BudgetInterface | null
+    periods: PeriodInterface[] | null
 }
