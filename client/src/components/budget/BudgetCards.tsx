3b541a1bb3577543a47fcde6be15bbbbf3621aaf
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-14 08:43:20 +0100

    BudgetCards created

---
+++ client/src/components/budget/BudgetCards.tsx
@@ -0,0 +1,5 @@
+import type { BudgetMiddlewarePropInterface } from "../../interfaces/budget";
+
+export default function BudgetCards({ budget }: BudgetMiddlewarePropInterface) {
+  return <div>BudgetCards</div>;
+}
