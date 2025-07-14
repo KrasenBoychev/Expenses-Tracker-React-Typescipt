856820d7a75d2a7c05b9ad361ae992e0269aa18d
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-14 18:41:35 +0100

    BudgetOverview accepts two values and has the correct interface

--- client/src/components/budget/BudgetOverview.tsx
+++ client/src/components/budget/BudgetOverview.tsx
@@ -1,8 +1,9 @@
-import type { BudgetMiddlewarePropInterface } from "../../interfaces/budget";
+import type { BudgetAndPeriodsPropsInterface } from "../../interfaces/budget";
 
 export default function BudgetOverview({
   budget,
-}: BudgetMiddlewarePropInterface) {
+  periods,
+}: BudgetAndPeriodsPropsInterface) {
   return (
     // <!-- Features -->
     <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
