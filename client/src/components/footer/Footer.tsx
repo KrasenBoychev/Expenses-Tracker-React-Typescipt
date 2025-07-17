e96ae2166a4def5e852e1a3b8b59ce15f43a6c6d
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-17 09:55:01 +0100

    Footer positions differently depending on the other components

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,6 +1,17 @@
+import { useLocation } from "react-router-dom";
+import { useAuthContext } from "../../contexts/AuthContext";
+
 export default function Footer() {
+  const { isAuthenticated } = useAuthContext();
+  const location = useLocation();
+  const path = location.pathname.split("/")[1];
+
   return (
-    <footer className="mt-auto text-center py-5">
+    <footer
+      className={`mt-auto ${
+        isAuthenticated && path !== "" && "ml-[300px]"
+      } text-center py-5`}
+    >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <p className="text-sm text-white/70">
           &copy; Expenses Tracker created by Krasen Boychev 2025
