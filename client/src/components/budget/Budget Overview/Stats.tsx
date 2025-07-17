63918a9af72da146d7a4ef8cc46d8fed383696e7
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-17 09:55:11 +0100

    Stats.tsx created

---
+++ client/src/components/budget/Budget Overview/Stats.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import type { SelectedPeriodPropInterface } from "../../../interfaces/budget";
+
+export default function Stats({ selectedPeriod }: SelectedPeriodPropInterface) {
+  const [plannedExpenses, setPlannedExpenses] = useState<number>(0);
+  const [savings, setSavings] = useState<number>(0);
+
+  useEffect(() => {
+    if (selectedPeriod && selectedPeriod.plannedExpenses.length > 0) {
+      let plannedExpensesCounter: number = 0;
+      selectedPeriod.plannedExpenses.forEach((expense) => {
+        plannedExpensesCounter += expense.value;
+      });
+      setPlannedExpenses(plannedExpensesCounter);
+      setSavings(selectedPeriod.income - plannedExpenses);
+    }
+  }, [selectedPeriod]);
+  return (
+    <div className="max-w-[85rem] py-20 mx-auto">
+      <div className="grid grid-cols-3 gap-30">
+        <div>
+          <h4 className="text-lg sm:text-xl font-semibold text-white uppercase">
+            Income
+          </h4>
+          <p className="mt-2 sm:mt-3 text-2xl sm:text-6xl font-bold text-blue-600">
+            {selectedPeriod?.income}
+          </p>
+        </div>
+        <div>
+          <h4 className="text-lg sm:text-xl font-semibold text-white uppercase">
+            Planned to spend
+          </h4>
+          <p className="mt-2 sm:mt-3 text-2xl sm:text-6xl font-bold text-blue-600">
+            {plannedExpenses}
+          </p>
+        </div>
+        <div>
+          <h4 className="text-lg sm:text-xl font-semibold text-white uppercase">
+            Planned to save
+          </h4>
+          <p className="mt-2 sm:mt-3 text-2xl sm:text-6xl font-bold text-blue-600">
+            {savings}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
