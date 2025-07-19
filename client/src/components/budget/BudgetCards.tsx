0799818a7a77f3b3201bda4f330803170d451233
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-19 10:22:22 +0100

    component structure created

--- client/src/components/budget/BudgetCards.tsx
+++ client/src/components/budget/BudgetCards.tsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
 import type { BudgetMiddlewarePropInterface } from "../../interfaces/budget";
 
 export default function BudgetCards({ budget }: BudgetMiddlewarePropInterface) {
-  return <div>BudgetCards</div>;
+  const [newCard, setNewCard] = useState<string>("");
+
+  const addNewCardHandler = () => {
+    //TODO
+  };
+  return (
+    <div className="flex flex-col w-full mt-10 ml-[150px] bg-gray-100 text-black">
+      <h3 className="w-[100%] p-2 text-xl text-center border-b-1 border-black">
+        All Cards
+      </h3>
+      <ul className="p-2">
+        <li className="flex justify-between gap-2">
+          <input
+            type="text"
+            className="w-[70%] h-7 rounded"
+            value={newCard}
+            onChange={(e) => setNewCard(e.target.value)}
+          />
+          <button
+            className="mr-2 py-1 px-2 bg-green-300 rounded"
+            onClick={addNewCardHandler}
+          >
+            add
+          </button>
+        </li>
+        {budget?.cards.map((card, index) => (
+          <li key={index}>{card}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
