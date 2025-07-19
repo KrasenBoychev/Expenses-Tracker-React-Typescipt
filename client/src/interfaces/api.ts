6ed5c0054ec5e721f406ee3e346861503602c6e0
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-19 15:03:21 +0100

    card and members types added to DataRequesterInterface

--- client/src/interfaces/api.ts
+++ client/src/interfaces/api.ts
@@ -1,9 +1,15 @@
 import type { FormErrorsInterface } from "./authentication";
+import type { CardInterface } from "./budget";
 
 export interface DataRequesterInterface extends FormErrorsInterface {
     budgetName?: string
     budgetsIds?: string[],
     budgetId?: string,
+    cardName?: string,
+    cardDetails?: CardInterface,
+    members?: string[],
+    memberEmail?: string,
+    memberId?: string,
     periodDate?: Date,
     periodsIds?: string[],
     periodId?: string,
