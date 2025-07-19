9e52d38d3ea3bc4de42fa7b9c046f12ba25f1e2f
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-19 10:23:49 +0100

    padding added to the parent div element

---
+++ client/src/components/budget/Budget Overview/Budget Details/ExpensesDetails.tsx
@@ -0,0 +1,20 @@
+import type { ExpensesPropInterface } from "../../../../interfaces/budget";
+
+export default function ExpensesDetails({ expenses }: ExpensesPropInterface) {
+  return (
+    <div className="w-[100%] py-2">
+      {expenses.length > 0 ? (
+        <ul className="px-2">
+          {expenses.map((expense, index) => (
+            <li key={index} className="flex justify-between g-1">
+              <div>{expense.expenseType}</div>
+              <div>{expense.value}</div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No expenses added yet</p>
+      )}
+    </div>
+  );
+}
