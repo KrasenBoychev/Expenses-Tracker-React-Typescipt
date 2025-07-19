5f4c2a6b01adb7730efec67232f052ae43faa773
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-19 15:02:16 +0100

    services for budget cards and members created

--- server/src/services/budget.js
+++ server/src/services/budget.js
@@ -25,8 +25,57 @@ async function createNewBudget(data) {
   return newBudget;
 }
 
+async function createNewCard(cardName, budgetId, ownerEmail) {
+  return await Budget.updateOne(
+    { _id: budgetId },
+    { $addToSet: { cards: { cardName, ownerEmail } } }
+  );
+}
+
+async function removeCard(cardDetails, budgetId) {
+  return await Budget.updateOne(
+    { _id: budgetId },
+    { $pull: { cards: cardDetails } }
+  );
+}
+
+async function getMembers(members) {
+  return await User.find({ _id: { $in: members } }).select("email");
+}
+
+async function addNewMember(memberEmail, budgetId) {
+  const existingUser = await User.findOne({ email: memberEmail });
+
+  if (existingUser) {
+    await Budget.updateOne(
+      { _id: budgetId },
+      { $addToSet: { members: existingUser._id } }
+    );
+
+    existingUser.budgets.push(budgetId);
+    existingUser.save();
+
+    return { _id: existingUser._id, email: existingUser.email };
+  } else {
+    throw new Error(`${memberEmail} does not exists!`);
+  }
+}
+
+async function removeMember(memberId, budgetId) {
+  await Budget.updateOne({ _id: budgetId }, { $pull: { members: memberId } });
+  return await User.updateOne(
+    { _id: memberId },
+    { $pull: { budgets: budgetId } }
+  );
+}
+
 module.exports = {
   getBudgets,
   getSingleBudget,
   createNewBudget,
+  createNewCard,
+  removeCard,
+  getMembers,
+  addNewMember,
+  removeMember,
 };
