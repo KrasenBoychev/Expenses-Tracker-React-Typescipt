4b65bcf99a0c01e5723e1af2f3a5ae6ac7ecb1ad
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-19 10:22:56 +0100

    Options.tsx created

---
+++ client/src/components/budget/Budget Overview/Options.tsx
@@ -0,0 +1,30 @@
+import type { OptionsPropsInterface } from "../../../interfaces/budget";
+
+export default function Options({
+  selectedPeriod,
+  setBudgetDetails,
+}: OptionsPropsInterface) {
+  const budgetDetailsHandler = () => {
+    setBudgetDetails(true);
+  };
+
+  const completeBudgetHandler = () => {};
+  return (
+    <div className="flex justify-center gap-40 my-5">
+      <button
+        className="py-3 px-5 bg-gray-400 rounded"
+        onClick={budgetDetailsHandler}
+      >
+        Details
+      </button>
+      {selectedPeriod?.endDate == null && (
+        <button
+          className="py-3 px-5 bg-red-500 rounded"
+          onClick={completeBudgetHandler}
+        >
+          Complete Period
+        </button>
+      )}
+    </div>
+  );
+}
