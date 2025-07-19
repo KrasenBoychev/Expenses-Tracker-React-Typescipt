4288f0c275cbbb89cba0e0c5ad36593e82a64597
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-19 10:24:46 +0100

    based on budgetDetails value, different components render

--- client/src/components/budget/Budget Overview/BudgetOverview.tsx
+++ client/src/components/budget/Budget Overview/BudgetOverview.tsx
@@ -5,6 +5,8 @@ import type {
 } from "../../../interfaces/budget";
 import SelectPeriod from "./SelectPeriod";
 import Stats from "./Stats";
+import Options from "./Options";
+import BudgetDetails from "./Budget Details/BudgetDetails";
 
 export default function BudgetOverview({
   budget,
@@ -14,10 +16,22 @@ export default function BudgetOverview({
     periods![0]
   );
 
+  const [budgetDetails, setBudgetDetails] = useState<boolean>(false);
+
   return (
     <div className="flex flex-col w-full mt-10 ml-[150px] text-center">
-      <SelectPeriod selectedPeriod={selectedPeriod} periods={periods} />
-      <Stats selectedPeriod={selectedPeriod} />
+      {budgetDetails ? (
+        <BudgetDetails selectedPeriod={selectedPeriod} />
+      ) : (
+        <>
+          <SelectPeriod selectedPeriod={selectedPeriod} periods={periods} />
+          <Stats selectedPeriod={selectedPeriod} />
+          <Options
+            selectedPeriod={selectedPeriod}
+            setBudgetDetails={setBudgetDetails}
+          />
+        </>
+      )}
     </div>
   );
 }
