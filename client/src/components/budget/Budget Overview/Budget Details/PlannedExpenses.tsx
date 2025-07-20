a33c7ae06071ce9741bf9beb6211245afdf236a6
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-20 19:17:25 +0100

    PlannedExpenses import Modal component and passes props to it

--- client/src/components/budget/Budget Overview/Budget Details/PlannedExpenses.tsx
+++ client/src/components/budget/Budget Overview/Budget Details/PlannedExpenses.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
-import type { PeriodInterface } from "../../../../interfaces/budget";
+import type {
+  ExpenseInterface,
+  PeriodInterface,
+} from "../../../../interfaces/budget";
 import toast from "react-hot-toast";
 import { addPlannedExpenseType } from "../../../../api/expenses-api";
+import Modal from "./Modal";
 
 interface PlannedExpensesProps {
   selectedPeriod: PeriodInterface | undefined;
@@ -21,7 +25,10 @@ export default function PlannedExpenses({
   const [newPlannedExpenseValue, setNewPlannedExpenseValue] = useState<
     number | string
   >("");
-  const [editExpense, setEditExpense] = useState<string>("");
+  const [editExpense, setEditExpense] = useState<ExpenseInterface | undefined>(
+    undefined
+  );
+  const [openModal, setOpenModal] = useState<boolean>(false);
 
   const addExpenseTypeHandler = async () => {
     if (newPlannedExpenseType == "" || newPlannedExpenseValue == "") {
@@ -63,73 +70,72 @@ export default function PlannedExpenses({
     }
   };
 
-  const editExpenseMouseHandler = (e: React.SyntheticEvent) => {
+  const editExpenseClickHandler = (e: React.SyntheticEvent) => {
     const target = e.target as typeof e.target & {
       id: string;
     };
-    setEditExpense(target.id);
+    console.log({ target });
+
+    const expenseDetails = selectedPeriod!.expenses.find(
+      (expense) => expense.expenseType == target.id
+    );
+    setEditExpense(expenseDetails);
+    setOpenModal(true);
   };
 
-  const editExpenseClickHandler = () => {};
-
   return (
-    <div className="w-[100%] py-2">
-      <ul className="px-2">
-        {selectedPeriod!.expenses.map((expense) => (
-          <li
-            key={expense.expenseType}
-            id={expense.expenseType}
-            className="flex justify-between g-2 hover:cursor-pointer"
-            onMouseEnter={editExpenseMouseHandler}
-            onMouseLeave={() => setEditExpense("")}
-            onClick={editExpenseClickHandler}
-          >
-            <div
-              className={`${
-                editExpense == expense.expenseType && "opacity-30"
-              }`}
-            >
-              {expense.expenseType}
-            </div>
-            <div
-              className={`${
-                editExpense == expense.expenseType && "opacity-30"
-              }`}
-            >
-              {expense.plannedExpenses.toFixed(2)}
-            </div>
-            {editExpense == expense.expenseType && (
-              <div>
-                <i className="fa-solid fa-pen-to-square"></i>
+    <>
+      <div className="w-[100%] py-2">
+        <ul className="px-2">
+          {selectedPeriod!.expenses.map((expense) => (
+            <li key={expense.expenseType} className="flex justify-between g-2">
+              <div className="flex justify-between w-[90%]">
+                <div>{expense.expenseType}</div>
+                <div>{expense.plannedExpenses.toFixed(2)}</div>
               </div>
-            )}
-          </li>
-        ))}
-        {selectedPeriod!.endDate == null && (
-          <li key={"newExpenseType"} className="flex justify-between mt-2">
-            <input
-              className="w-[45%] h-[25px] pl-1"
-              type="text"
-              placeholder="type"
-              value={newPlannedExpenseType}
-              onChange={(e) => setNewPlannedExpenseType(e.target.value)}
-            />
-            <input
-              className="w-[45%] h-[25px] pl-1"
-              type="number"
-              placeholder="value"
-              value={newPlannedExpenseValue}
-              onChange={(e) => setNewPlannedExpenseValue(e.target.value)}
-            />
-            <button
-              className="px-1.5 bg-green-300 rounded"
-              onClick={addExpenseTypeHandler}
-            >
-              <i className="fa-solid fa-plus"></i>
-            </button>
-          </li>
-        )}
-      </ul>
-    </div>
+              <div>
+                <i
+                  className="fa-solid fa-pen-to-square hover:cursor-pointer"
+                  id={expense.expenseType}
+                  onClick={editExpenseClickHandler}
+                ></i>
+              </div>
+            </li>
+          ))}
+          {selectedPeriod!.endDate == null && (
+            <li key={"newExpenseType"} className="flex justify-between mt-2">
+              <input
+                className="w-[45%] h-[25px] pl-1"
+                type="text"
+                placeholder="type"
+                value={newPlannedExpenseType}
+                onChange={(e) => setNewPlannedExpenseType(e.target.value)}
+              />
+              <input
+                className="w-[45%] h-[25px] pl-1"
+                type="number"
+                placeholder="value"
+                value={newPlannedExpenseValue}
+                onChange={(e) => setNewPlannedExpenseValue(e.target.value)}
+              />
+              <button
+                className="px-1.5 bg-green-300 rounded"
+                onClick={addExpenseTypeHandler}
+              >
+                <i className="fa-solid fa-plus"></i>
+              </button>
+            </li>
+          )}
+        </ul>
+      </div>
+      {openModal && (
+        <Modal
+          setOpenModal={setOpenModal}
+          editExpense={editExpense}
+          periodId={selectedPeriod!._id}
+          setPeriods={setPeriods}
+        />
+      )}
+    </>
   );
 }
