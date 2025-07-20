8f7ff6fc8ec973cb47b763633f16ccfe33d096f1
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-20 19:45:20 +0100

    addValueHandler logic completed

--- client/src/components/budget/Add Income&Expenses/AddExpense.tsx
+++ client/src/components/budget/Add Income&Expenses/AddExpense.tsx
@@ -1,17 +1,52 @@
 import { useState } from "react";
-import type { PeriodInterface } from "../../../../../interfaces/budget";
+import type { PeriodInterface } from "../../../interfaces/budget";
+import toast from "react-hot-toast";
+import { addActualExpense } from "../../../api/expenses-api";
 
 interface AddExpenseProps {
   currPeriod: PeriodInterface | undefined;
+  setPeriods: Function;
 }
 
-export default function AddExpense({ currPeriod }: AddExpenseProps) {
+export default function AddExpense({
+  currPeriod,
+  setPeriods,
+}: AddExpenseProps) {
   const [inputValue, setInputValue] = useState<number | string>("0");
   const [selectedExpenseType, setSelectedExpenseType] = useState<string>(
     currPeriod!.expenses.length > 0 ? currPeriod!.expenses[0].expenseType : ""
   );
 
-  const addValueHandler = async () => {};
+  const addValueHandler = async () => {
+    if (inputValue == "" || Number(inputValue) <= 0) {
+      return;
+    }
+    const expenseToAdd = Number(inputValue);
+
+    try {
+      await addActualExpense(
+        currPeriod!._id,
+        selectedExpenseType,
+        expenseToAdd
+      );
+      setPeriods((periods: PeriodInterface[]) => {
+        const findPeriod = periods.find(
+          (period) => period._id == currPeriod!._id
+        );
+        const findExpense = findPeriod?.expenses.find(
+          (expense) => expense.expenseType == selectedExpenseType
+        );
+        findExpense!.actualExpenses += Number(expenseToAdd);
+        return periods;
+      });
+
+      setInputValue("");
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        return toast.error(e.message);
+      }
+    }
+  };
   return (
     <div>
       <div className="flex flex-col justify-self-center gap-8 w-[70%] p-7 bg-white rounded">
