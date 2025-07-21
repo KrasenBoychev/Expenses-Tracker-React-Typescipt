9211aa2ec692cabea58fb8d792fd0dc2c388ba17
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-21 19:07:15 +0100

    "Match red values" and  "Match all values" added; matchRedValuesHandler and matchAllValuesHandler created;

--- client/src/components/budget/Budget Overview/Budget Details/ActualExpenses.tsx
+++ client/src/components/budget/Budget Overview/Budget Details/ActualExpenses.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
 import type { PeriodInterface } from "../../../../interfaces/budget";
+import { matchExpensesValues } from "../../../../api/expenses-api";
+import toast from "react-hot-toast";
 
 interface ActualExpensesProps {
   selectedPeriod: PeriodInterface | undefined;
@@ -12,25 +13,79 @@ export default function ActualExpenses({
   setPeriods,
   expenseChangeDetection,
 }: ActualExpensesProps) {
+  const matchRedValuesHandler = async () => {
+    const expensesTypes: string[] = [];
+    selectedPeriod?.expenses.forEach((expense) => {
+      if (expense.actualExpenses > expense.plannedExpenses) {
+        expensesTypes.push(expense.expenseType);
+      }
+    });
+
+    try {
+      const updatedPeriod = await matchExpensesValues(
+        selectedPeriod!._id,
+        expensesTypes
+      );
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        return toast.error(e.message);
+      }
+    }
+  };
+
+  const matchAllValuesHandler = async () => {
+    const expensesTypes = selectedPeriod!.expenses.map(
+      (expense) => expense.expenseType
+    );
+
+    try {
+      const updatedPeriod = await matchExpensesValues(
+        selectedPeriod!._id,
+        expensesTypes
+      );
+
+      
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        return toast.error(e.message);
+      }
+    }
+  };
   return (
     <div className="w-[100%] py-2">
       {(expenseChangeDetection == true || expenseChangeDetection == false) &&
       selectedPeriod!.expenses.length > 0 ? (
-        <ul className="px-2">
-          {selectedPeriod!.expenses.map((expense, index) => (
-            <li
-              key={index}
-              className={`flex justify-between g-1 p-1 ${
-                expense.actualExpenses <= expense.plannedExpenses
-                  ? "bg-green-200"
-                  : "bg-red-200"
-              }`}
-            >
-              <div>{expense.expenseType}</div>
-              <div>{expense.actualExpenses.toFixed(2)}</div>
+        <>
+          <ul className="px-2">
+            {selectedPeriod!.expenses.map((expense, index) => (
+              <li
+                key={index}
+                className={`flex justify-between g-1 p-1 ${
+                  expense.actualExpenses <= expense.plannedExpenses
+                    ? "bg-green-200"
+                    : "bg-red-200"
+                }`}
+              >
+                <div>{expense.expenseType}</div>
+                <div>{expense.actualExpenses.toFixed(2)}</div>
+              </li>
+            ))}
+            <li className="flex justify-between mt-1 p-1">
+              <button
+                className="w-[40%] text-red-600 underline"
+                onClick={matchRedValuesHandler}
+              >
+                Match red values
+              </button>
+              <button
+                className="w-[40%] underline"
+                onClick={matchAllValuesHandler}
+              >
+                Match all values
+              </button>
             </li>
-          ))}
-        </ul>
+          </ul>
+        </>
       ) : (
         <p>No expenses added yet</p>
       )}
