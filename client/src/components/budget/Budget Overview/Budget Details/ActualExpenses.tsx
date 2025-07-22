4c8d706dc77b8850e9c0e6dc068aff98175779a8
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-22 18:55:05 +0100

    when expenseChangeDetection boolean value changes, the match values btns in ActualExpenses can be rerendered

--- client/src/components/budget/Budget Overview/Budget Details/ActualExpenses.tsx
+++ client/src/components/budget/Budget Overview/Budget Details/ActualExpenses.tsx
@@ -1,6 +1,8 @@
 import type { PeriodInterface } from "../../../../interfaces/budget";
 import { matchExpensesValues } from "../../../../api/expenses-api";
 import toast from "react-hot-toast";
+import BudgetAddIncomeExpense from "../../Add Income&Expenses/BudgetAddIncomeExpense";
+import { useEffect, useState } from "react";
 
 interface ActualExpensesProps {
   selectedPeriod: PeriodInterface | undefined;
@@ -17,6 +19,25 @@ export default function ActualExpenses({
   setExpenseChangeDetection,
   setUpdateTotalSavings,
 }: ActualExpensesProps) {
+  const [redValuesCounter, setRedValuesCounter] = useState<number>(0);
+  const [otherValuesCounter, setOtherValuesCounter] = useState<number>(0);
+
+  useEffect(() => {
+    let redValues = 0;
+    let otherValues = 0;
+
+    selectedPeriod!.expenses.forEach((expense) => {
+      if (expense.actualExpenses > expense.plannedExpenses) {
+        redValues += 1;
+      } else if (expense.actualExpenses < expense.plannedExpenses) {
+        otherValues += 1;
+      }
+    });
+
+    setRedValuesCounter(redValues);
+    setOtherValuesCounter(otherValues);
+  }, [expenseChangeDetection]);
+
   const matchValuesHandler = async (e: React.SyntheticEvent) => {
     const target = e.target as typeof e.target & {
       id: string;
@@ -63,9 +84,9 @@ export default function ActualExpenses({
       selectedPeriod!.expenses.length > 0 ? (
         <>
           <ul className="px-2">
-            {selectedPeriod!.expenses.map((expense, index) => (
+            {selectedPeriod!.expenses.map((expense) => (
               <li
-                key={index}
+                key={expense.expenseType}
                 className={`flex justify-between g-1 p-1 ${
                   expense.actualExpenses <= expense.plannedExpenses
                     ? "bg-green-200"
@@ -77,20 +98,25 @@ export default function ActualExpenses({
               </li>
             ))}
             <li className="flex justify-between mt-1 p-1">
-              <button
-                className="w-[40%] text-red-600 underline"
-                id="redValues"
-                onClick={matchValuesHandler}
-              >
-                Match red values
-              </button>
-              <button
-                className="w-[40%] underline"
-                id="allValues"
-                onClick={matchValuesHandler}
-              >
-                Match all values
-              </button>
+              {redValuesCounter > 0 && (
+                <button
+                  className="w-[40%] text-red-600 underline"
+                  id="redValues"
+                  onClick={matchValuesHandler}
+                >
+                  Match red values
+                </button>
+              )}
+
+              {otherValuesCounter > 0 && (
+                <button
+                  className="w-[40%] underline"
+                  id="allValues"
+                  onClick={matchValuesHandler}
+                >
+                  Match all values
+                </button>
+              )}
             </li>
           </ul>
         </>
