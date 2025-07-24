ea40ed5c3d3a2db7d601633e7dabfee8972221b1
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-24 19:55:53 +0100

    Footer.test.tsx deleted

--- client/src/tests/Footer.test.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOMClient from "react-dom/client";
-import { it } from "vitest";
-import Footer from "../components/footer/Footer";
-
-it("renders Footer without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOMClient.createRoot(div).render(<Footer />);
-});
