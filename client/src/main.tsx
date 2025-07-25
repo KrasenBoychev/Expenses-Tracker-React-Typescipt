3e797d8c71f52cc835cfc951e923de053480e852
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-25 17:59:56 +0100

    Error Boundary installed and implemented in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <ErrorBoundary
+      fallback={
+        <div className="flex justify-center mt-20 text-2xl font-bold uppercase">
+          Something went wrong
+        </div>
+      }
+    >
       <App />
-    </BrowserRouter>
+    </ErrorBoundary>
+  </BrowserRouter>
   // </StrictMode>
 );
