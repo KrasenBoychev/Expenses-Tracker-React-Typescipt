18647ed77890f5b2c9280f03c8d704372545a6e0
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-28 11:10:11 +0100

    tests created for BudgetMemebers.tsx

--- client/src/tests/components/budget/BudgetMembers.test.tsx
+++ client/src/tests/components/budget/BudgetMembers.test.tsx
@@ -1,14 +1,63 @@
 import { it, expect } from "vitest";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import BudgetMembers from "../../../components/budget/BudgetMembers";
+import type { BudgetInterface } from "../../../interfaces/budget";
 
-const BudgetMembersComponent = (
-  <BrowserRouter>
-    <BudgetMembers budget={null} setBudget={() => {}} />
-  </BrowserRouter>
-);
+const noBudget = null;
+const hasBudget: BudgetInterface = {
+  _id: "000",
+  budgetName: "test",
+  cards: [],
+  members: ["68712707cbac7c3410e9408c"],
+  allPeriods: [],
+};
 
-it("renders 'BudgetMembers' without crashing", () => {
-  render(BudgetMembersComponent);
+const BudgetMembersComponent = (budget: BudgetInterface | null) => {
+  return (
+    <BrowserRouter>
+      <BudgetMembers budget={budget} setBudget={() => {}} />
+    </BrowserRouter>
+  );
+};
+
+it("renders 'BudgetMembers' with budget value 'null' without crashing", () => {
+  render(BudgetMembersComponent(noBudget));
+});
+
+it("renders 'BudgetMembers' with budget value 'object' without crashing", () => {
+  render(BudgetMembersComponent(hasBudget));
+});
+
+it("renders 'All Members' heading", () => {
+  render(BudgetMembersComponent(noBudget));
+  expect(screen.getByText("All Members")).toBeVisible();
+});
+
+it("renders input field and button", () => {
+  render(BudgetMembersComponent(noBudget));
+  expect(screen.getByTestId("budget-members-input")).toBeVisible();
+  expect(screen.getByTestId("budget-members-button")).toBeVisible();
+});
+
+it("renders only one 'li' element if the budget value is 'null'", () => {
+  render(BudgetMembersComponent(noBudget));
+  expect(screen.getByTestId("budget-members-ul").childElementCount).toEqual(1);
+});
+
+it("renders two 'li' elements if the budget has one member", async () => {
+  render(BudgetMembersComponent(hasBudget));
+  await waitFor(() => {
+    expect(screen.getByText("k@gmail.com")).toBeVisible();
+    expect(screen.getByTestId("budget-members-ul").childElementCount).toEqual(
+      2
+    );
+  });
+});
+
+it("renders the new member when 'add' button is clicked", async () => {
+  render(BudgetMembersComponent(hasBudget));
+  await waitFor(() => {
+    
+  });
 });
