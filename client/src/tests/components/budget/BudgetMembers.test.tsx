ccc2aa7b16f7284bbf53a8d509c6067108aac389
Krasen Boychev <krasenboychev11@gmail.com>
2025-07-28 12:44:45 +0100

    more tests added for BudgetMembers.tsx

--- client/src/tests/components/budget/BudgetMembers.test.tsx
+++ client/src/tests/components/budget/BudgetMembers.test.tsx
@@ -1,13 +1,14 @@
 import { it, expect } from "vitest";
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import BudgetMembers from "../../../components/budget/BudgetMembers";
 import type { BudgetInterface } from "../../../interfaces/budget";
+import { act } from "react";
 
 const noBudget = null;
 const hasBudget: BudgetInterface = {
-  _id: "000",
-  budgetName: "test",
+  _id: "6883be1c10ed396049b94d8f",
+  budgetName: "Family Budget",
   cards: [],
   members: ["68712707cbac7c3410e9408c"],
   allPeriods: [],
@@ -57,7 +58,51 @@ it("renders two 'li' elements if the budget has one member", async () => {
 
 it("renders the new member when 'add' button is clicked", async () => {
   render(BudgetMembersComponent(hasBudget));
-  await waitFor(() => {
-    
+
+  const input = screen.getByTestId("budget-members-input");
+  const addButton = screen.getByTestId("budget-members-button");
+
+  await act(async () => {
+    fireEvent.change(input, { target: { value: "test@gmail.com" } });
+    fireEvent.click(addButton);
+
+    await fetch("http://localhost:5000/budget/members/addNewMember", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        memberEmail: "test@gmail.com",
+        budgetId: "6883be1c10ed396049b94d8f",
+      }),
+    });
   });
+
+  expect(screen.getByText("test@gmail.com")).toBeVisible();
+});
+
+it("removes a member when 'remove' button is clicked", async () => {
+  hasBudget.members.push("6887279090475e736ad08089");
+  render(BudgetMembersComponent(hasBudget));
+
+  await waitFor(async () => {
+    const removeButton = screen.getByTestId("6887279090475e736ad08089");
+
+    await act(async () => {
+      fireEvent.click(removeButton);
+
+      await fetch("http://localhost:5000/budget/members/removeMember", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          memberId: "6887279090475e736ad08089",
+          budgetId: "6883be1c10ed396049b94d8f",
+        }),
+      });
+    });
+  });
+
+  expect(screen.queryByTestId("test@gmail.com")).not.toBeInTheDocument();
 });
